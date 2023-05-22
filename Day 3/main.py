a5d9e22f047e4798e462087eeb825abb00250c96
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-22 10:21:44 +0300

    Day 3: - removed unnecessary if

--- Day 3/main.py
+++ Day 3/main.py
@@ -70,23 +70,22 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 
 #Write your code below this line 👇
 
-if size:
-    bill = 0
+bill = 0
+if size == 'S':
+       bill = 15
+elif size == 'M':
+    bill = 20
+else:
+    bill = 25
+
+if add_pepperoni == 'Y':
     if size == 'S':
-        bill = 15
-    elif size == 'M':
-        bill = 20
+        bill += 2
     else:
-        bill = 25
+         bill +=3
 
-    if add_pepperoni == 'Y':
-        if size == 'S':
-            bill += 2
-        else:
-            bill +=3
+if extra_cheese == 'Y':
+    bill +=1
 
-    if extra_cheese == 'Y':
-        bill +=1
-
-    print(f'Your final bill is: ${bill}.')
+print(f'Your final bill is: ${bill}.')
 
