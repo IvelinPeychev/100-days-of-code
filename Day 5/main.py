9a05982833c07724051d33fcf46110f2bf32d1bf
Ivelin Peychev <peychev.vn@gmail.com>
2023-05-23 10:14:58 +0300

    Day 5: - Solving tasks

--- Day 5/main.py
+++ Day 5/main.py
@@ -18,3 +18,20 @@ for student in student_heights:
 
 average = round(total / count)
 print(average)
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+max_number = 0
+
+for number in student_scores:
+    if number > max_number:
+        max_number = number
+
+print(f'The highest score in the class is: {max_number}')
