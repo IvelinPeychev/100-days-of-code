23550386a84cf23b8ac93d690f0a0a13e72a4bfa
Ivelin Peychev <peychev.vn@gmail.com>
2023-06-03 21:15:11 +0300

    Day 21: - initial commit - implementing the project - added file regarding inheritance - list slicing

---
+++ Day 21 - I - Snake game part 2 - Inheritence and List Slicing/test.py
@@ -0,0 +1,40 @@
+class Dog:
+    def __init__(self):
+        self.temperament = "loyal"
+
+    def bark(self):
+        print("Woof, woof!")
+
+
+class Labrador(Dog):
+    def __init__(self):
+        super().__init__()
+        # self.temperament = "friendly"
+
+
+buddy = Labrador()
+buddy.bark()
+print(buddy.temperament)
+
+
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        # super().__init__()
+        self.num = 1
+
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater.")
+
+    def swim(self):
+        print("moving in water.")
+
+nemo = Fish()
+nemo.breathe()
